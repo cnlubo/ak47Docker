a4bfcb7309b3a212d711dd9f43d9dc968eef25f1
cnlubo <bolu20080314@gmail.com>
2021-10-14 18:22:41 +0800

    update

--- k3s/11-deploy-ingress-nginx.sh
+++ k3s/11-deploy-ingress-nginx.sh
@@ -8,19 +8,20 @@ echo "##########################################################################
 WORKERS=$(echo $(multipass list | grep worker | awk '{print $1}'))
 for WORKER in ${WORKERS}; do
     echo -e "[${LB}Info${NC}] deploy images on ${WORKER}"
-    multipass  transfer addons/ingress-nginx/1.0.3/ingress-nginx-controller.tar "${WORKER}":
-    multipass  transfer addons/ingress-nginx/1.0.3/ingress-nginx-kube-webhook-certgen.tar "${WORKER}":
+    multipass transfer addons/ingress-nginx/1.0.3/ingress-nginx-controller.tar "${WORKER}":
+    multipass transfer addons/ingress-nginx/1.0.3/ingress-nginx-kube-webhook-certgen.tar "${WORKER}":
     multipass exec "${WORKER}" -- /bin/bash -c "sudo ctr -n=k8s.io images import ingress-nginx-controller.tar" | grep -w "unpacking"
     multipass exec "${WORKER}" -- /bin/bash -c "sudo ctr -n=k8s.io images import ingress-nginx-kube-webhook-certgen.tar" | grep -w "unpacking"
     multipass exec "${WORKER}" -- /bin/bash -c "sudo crictl images" | grep -w "ingress-nginx"
-   
+
 done
 sleep 5
 #export KUBECONFIG=$(pwd)/k3s.yaml
-kubectl create ns ingress-nginx
+# kubectl create ns ingress-nginx
+# 默认开启 hostNetwork
 kubectl create -f addons/ingress-nginx/1.0.3/deploy.yaml
 
 sleep 10
 POD_NAMESPACE=ingress-nginx
 POD_NAME=$(kubectl get pods -n $POD_NAMESPACE -l app.kubernetes.io/name=ingress-nginx --field-selector=status.phase=Running -o jsonpath='{.items[0].metadata.name}')
-kubectl exec -it $POD_NAME -n $POD_NAMESPACE -- /nginx-ingress-controller --version
+kubectl exec -it "$POD_NAME" -n $POD_NAMESPACE -- /nginx-ingress-controller --version
