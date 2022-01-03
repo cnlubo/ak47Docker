d78f6c5b81cf41a3e5432bcadab493c745c39f1e
lubo <454331202@qq.com>
2022-01-04 00:09:36 +0800

    update

--- k3s/3-deploy-metal-lb.sh
+++ k3s/3-deploy-metal-lb.sh
@@ -4,7 +4,6 @@
 # @Date: 2021-10-10 11:27:35
 # @LastEditTime: 2021-12-30 15:10:30
 # @LastEditors: Please set LastEditors
-# @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 # @FilePath: /ak47Docker/k3s/3-install-metal-lb.sh
 ###
 GREEN='\033[0;32m'
