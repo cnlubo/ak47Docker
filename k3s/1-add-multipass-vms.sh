9956f3020e0bf9ac9671101e39df45a9584c05cc
lubo <454331202@qq.com>
2022-01-17 18:13:51 +0800

    update

--- k3s/1-add-multipass-vms.sh
+++ k3s/1-add-multipass-vms.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+###
+ # @Author: your name
+ # @Date: 2022-01-17 13:57:59
+ # @LastEditTime: 2022-01-17 14:05:57
+ # @LastEditors: Please set LastEditors
+ # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ # @FilePath: /ak47Docker/k3s/1-add-multipass-vms.sh
+### 
 # check if required applications and files are available
 #./utils/dependency-check.sh
 
@@ -22,18 +30,23 @@ begin_num=$(($num_Worker + 1))
 end_num=$(($num_Worker + $nodeCount))
 WORKER=$(eval 'echo k3s-worker{'"$begin_num"'..'"$end_num"'}')
 NODES+=$WORKER
+echo $NODES
+echo $cpuCount
+echo $memCount
+echo $diskCount
+echo $OSversion
 # # Create containers
 for NODE in ${NODES}; do
-  echo "############################################################################"
-  echo "Deploy multipass vm $NODE"
-  multipass launch --name ${NODE} --cpus ${cpuCount} --mem ${memCount}G --disk ${diskCount}G --cloud-init cloud-config.yaml "$OSversion"
+    echo "############################################################################"
+    echo "Deploy multipass vm $NODE"
+    multipass launch --name ${NODE} --cpus ${cpuCount} --mem ${memCount}G --disk ${diskCount}G --cloud-init cloud-config.yaml "$OSversion"
 done
 
 # Wait a few seconds for nodes to be up
-sleep 5
+sleep 10
 
 for NODE in ${NODES}; do
-  multipass transfer ~/.ssh/id_rsa.pub ${NODE}:
-  multipass exec ${NODE} -- sudo iptables -P FORWARD ACCEPT
-  multipass exec ${NODE} -- bash -c 'sudo cat /home/ubuntu/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys'
+    multipass transfer ~/.ssh/id_rsa.pub ${NODE}:
+    multipass exec ${NODE} -- sudo iptables -P FORWARD ACCEPT
+    multipass exec ${NODE} -- bash -c 'sudo cat /home/ubuntu/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys'
 done
