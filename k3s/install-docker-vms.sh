2cdf2a5b89ed6210bdeb5b50ad864287ddbc25d0
cnak47 <bolu20080314@gmail.com>
2022-09-06 18:16:19 +0800

    update

--- k3s/install-docker-vms.sh
+++ k3s/install-docker-vms.sh
@@ -3,7 +3,7 @@
 # Author: cnak47
 # Date: 2022-04-29 17:02:24
 # LastEditors: cnak47
-# LastEditTime: 2022-05-01 08:49:56
+# LastEditTime: 2022-09-06 17:39:48
 # FilePath: /docker_workspace/ak47Docker/k3s/install-docker-vms.sh
 # Description:
 #
@@ -24,17 +24,33 @@ source "$ScriptPath"/include/color.sh
 # shellcheck disable=SC1091
 source "$ScriptPath"/include/common.sh
 SOURCE_SCRIPT "${scriptdir:?}"/options.conf
-
+install_method="manual"
 INFO_MSG "$MODULE" "Create mup-docker VM "
 cpuCount=2
 memCount=4
-diskCount=10
-multipass launch --name mup-docker \
-    --cpus ${cpuCount} \
-    --mem ${memCount}G \
-    --disk ${diskCount}G \
-    --cloud-init docker-config.yaml \
-    "$OSversion"
+diskCount=5
+dockerdata="/Users/ak47/workerspace/docker_workspace/dockerdata"
+if [ ! -d $dockerdata ]; then
+    mkdir -p $dockerdata
+fi
+if [ $install_method = "auto" ]; then
+    multipass launch --name mup-docker \
+        --cpus ${cpuCount} \
+        --mem ${memCount}G \
+        --disk ${diskCount}G \
+        --cloud-init docker-config.yaml \
+        --timeout 1200 \
+        "$OSversion"
+else
+    multipass launch --name mup-docker \
+        --cpus ${cpuCount} \
+        --mem ${memCount}G \
+        --disk ${diskCount}G \
+        --mount $dockerdata:"/home/docker" \
+        --cloud-init docker-config-manual.yaml \
+        --timeout 1200 \
+        "$OSversion"
+fi
 sleep 10
 multipass ls
 INFO_MSG "$MODULE" "Please add below line to .zshrc"
